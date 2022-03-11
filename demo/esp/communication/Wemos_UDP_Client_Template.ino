497f8c46f33b4699c2910a686cd8fd64e87ef619
botabotlab <61827555+botabotlab@users.noreply.github.com>
2022-03-11 15:13:29 +0100

    Create Wemos_UDP_Client_Template.ino

---
+++ demo/esp/communication/Wemos_UDP_Client_Template.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiUdp.h>
+
+WiFiClient client;
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+const char* HostName = "ESP8266_MQTT_HTTP_DEMO";
+
+#define UDP_PORT 2891
+#define UDP_SERVER "192.168.178.100"
+
+WiFiUDP UDP;
+char ping[] = "PING";
+
+//WIFI SETUP=============================
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname(HostName);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address : ");
+  Serial.println(WiFi.localIP());
+}
+//WIFI SETUP=============================
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+
+  //PING---------------------------------
+  UDP.beginPacket(UDP_SERVER, UDP_PORT);
+  UDP.write(ping);
+  UDP.endPacket();
+  //PING---------------------------------
+}
+
+void loop() {
+}
