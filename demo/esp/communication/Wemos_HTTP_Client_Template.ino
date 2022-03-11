3f0e063709a84075043344eab997ec25268f9cb7
botabotlab <61827555+botabotlab@users.noreply.github.com>
2022-03-11 15:18:08 +0100

    Create Wemos_HTTP_Client_Template.ino

---
+++ demo/esp/communication/Wemos_HTTP_Client_Template.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+WiFiClient client;
+HTTPClient http;
+
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+const char* HostName = "ESP8266_HTTP_CLIENT_DEMO";
+String server = "http://192.168.178.100:2880/remote/test";
+
+//WIFI SETUP=============================
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname(HostName);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address : ");
+  Serial.println(WiFi.localIP());
+}
+//WIFI SETUP=============================
+
+//PING=======================================
+int ping() {
+  http.begin(client, server.c_str());
+  int httpResponseCode = http.POST("PING");
+  Serial.println("PING");
+  Serial.print("Response : ");
+  Serial.println(httpResponseCode);
+  String respayload = http.getString();
+  Serial.println(respayload);
+  http.end();
+  return(httpResponseCode);
+}
+//PING=======================================
+
+//SETUP==================
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  ping();
+}
+//SETUP==================
+
+//LOOP========
+void loop() {
+}
+//LOOP========
